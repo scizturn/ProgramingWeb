2c053c87d735b6f0afbecb68808141029b644fe7
Scizturn <76981875+scizturn@users.noreply.github.com>
2023-03-15 22:39:31 +0700

    Input Pertemuan 1 - 3

---
+++ Pertemuan 1/MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass{
+      public static void main (String[]args){
+      String firstName = "Tegar ";
+      String secondName = "Ardyansyah";
+      String fullName = firstName + secondName;
+      System.out.println(fullName);
+     }
+}
