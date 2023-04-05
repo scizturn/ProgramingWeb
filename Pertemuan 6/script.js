82949e7414952c78a3994f81090621e080a15318
Scizturn <76981875+scizturn@users.noreply.github.com>
2023-04-05 17:54:32 +0700

    Up pertemuan 6

---
+++ Pertemuan 6/script.js
@@ -0,0 +1,25 @@
+const penjumlahan = 2 + 3;
+console.log(penjumlahan); // 5
+
+const pengurangan = 3 - 2;
+console.log(pengurangan); // 1
+
+const perkalian = 3 * 2;
+console.log(perkalian); // 6
+
+const pembagian = 5 * 2;
+console.log(pembagian); // 10
+
+const modulus = 10 % 3;
+console.log(modulus); // 1 - karena 10 dibagi 3 sisanya 1
+
+const eksponensial = 5 ** 3; // 5 pangkat 3
+console.log(eksponensial); // 125
+
+let increment = 5;
+increment++; // 5 + 1
+console.log(increment); // 6
+
+let decrement = 5;
+decrement--; // 5 - 1
+console.log(decrement); // 4
